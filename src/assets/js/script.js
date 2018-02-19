09f9501c1b50d2305926bd7550d9f68730c98069
Nakama <ichsanwahyudi>
2018-02-19 17:36:50 +0700

    updated page my rock journey

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -61,4 +61,52 @@ $(document).ready(function(){
     $(this).addClass('active');
   });
 
+  $(".join-activity__input-file").click(function() {
+    $("#input-file").click();
+  })
+
+  $("#input-file").change(function(e){
+    console.log(e)
+    var file = this.files[0];
+    if (typeof file !== "undefined") {
+      var imgType = file.type;
+      var imgSize = file.size;
+      var match = ["image/png","image/jpg","image/jpeg"];
+
+      if (!((imgType == match[0]) || (imgType == match[1]) || (imgType == match[2]) )) {
+        return false;
+      }else{
+        console.log(imgSize);
+        if (imgSize > 1000000) {
+        }else{
+          readFile(this);
+        }
+      }
+    }
+  });
+
+  function readFile(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = imagePreview;
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  function imagePreview(e) {
+    console.log(e)
+    $('.join-activity__input-file').css({
+        background: 'url('+e.target.result+')',
+        // 'backgrund-position': 'center'
+    });
+    $(".join-activity__input-file img, .join-activity__input-file span").css('opacity','0')
+  }
+
+  $("#btn-trigger-modal").click(function() {
+    $("#modal-thankyou").css({
+      'display': 'flex !important'
+    })
+  })
+
+
 })
